80e0bb67f1600132f548fa4cb2ef976cd3a24ccd
Rafael <rafacarlossilver@yahoo.com.br>
2018-05-25 16:49:10 -0300

    adicionando porcentagens

---
+++ get_game_result.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: windows-1252 -*-
+
+import pandas as pd
+import csv
+
+
+
+
+
+def estatisticasTime(time,pais):
+    import numpy as np
+    vitorias = time[time['winner'] == pais].count()
+    derrotas = time[time['winner'] != pais].count()
+    partidas = time.date.count()
+    golsEmcasa = time.home_score.sum()
+    golsForaDeCasa = time.away_score.sum()
+    totalGolsPorPartida =  golsForaDeCasa + golsEmcasa
+    mediaGols = totalGolsPorPartida / partidas
+    aproveitamento = 100 * float (vitorias.date ) /float (partidas)
+    estatisticas = {"partidas":partidas,"aproveitamento":round(aproveitamento,3), "time":pais,"gols": mediaGols, "vitorias":vitorias.date,"derrotas":derrotas.date}
+
+    return estatisticas;
+
+
+df = pd.read_csv('results.csv') #partidas internacionais desde 1800
+dfwc = pd.read_csv('worldcup2018.csv')
+
+
+#verifica vencedor
+def winner(row):
+    if row['home_score'] > row['away_score']: return row['home_team']
+    elif row['home_score'] < row['away_score']: return row['away_team']
+    else: return 'DRAW'
+
+df['winner'] = df.apply(lambda row: winner(row), axis=1)
+
+
+#verifica perdedor
+def loser(row):
+    if row['home_score'] < row['away_score']: return row['home_team']
+    elif row['home_score'] > row['away_score']: return row['away_team']
+    else: return 'DRAW'
+
+df['loser'] = df.apply(lambda row: loser(row), axis=1)
+
+#separando os ultimos resultados
+
+#print(dfwc)
+
+
+with open('worldcup2018.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+
+        pais = row[0]
+        print(pais)
+        timeEmcasa = df[df['home_team'] == pais ]
+        timeForaCasa = df[df['away_team'] == pais ]
+        time = pd.concat([timeEmcasa,timeForaCasa])
+        if time.date.count() == 0:
+            continue;
+        print(estatisticasTime(time,pais))
