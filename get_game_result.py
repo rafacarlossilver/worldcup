8ac6e6d5cf821e8b900273898f1ac7cc088440cb
Rafael <rafacarlossilver@yahoo.com.br>
2018-06-12 14:33:08 -0300

    ajustando as estatisticas

--- get_game_result.py
+++ get_game_result.py
@@ -4,22 +4,7 @@
 import pandas as pd
 import csv
 import sys
-
-
-
-def estatisticasTime(time,pais):
-    import numpy as np
-    vitorias = time[time['winner'] == pais].count()
-    derrotas = time[time['winner'] != pais].count()
-    partidas = time.date.count()
-    golsEmcasa = time.home_score.sum()
-    golsForaDeCasa = time.away_score.sum()
-    totalGolsPorPartida =  golsForaDeCasa + golsEmcasa
-    mediaGols = totalGolsPorPartida / partidas
-    aproveitamento = 100 * float (vitorias.date ) /float (partidas)
-    estatisticas = {"partidas":partidas,"aproveitamento":round(aproveitamento,3), "time":pais,"gols": mediaGols, "vitorias":vitorias.date,"derrotas":derrotas.date}
-
-    return estatisticas;
+import statistics
 
 
 df = pd.read_csv('results.csv') #partidas internacionais desde 1800
@@ -28,35 +13,72 @@ dfwc = pd.read_csv('worldcup2018.csv')
 
 #verifica vencedor
 def winner(row):
-    if row['home_score'] > row['away_score']: return row['home_team']
-    elif row['home_score'] < row['away_score']: return row['away_team']
-    else: return 'DRAW'
+    if row['home_score'] > row['away_score']: return 1
+    elif row['home_score'] < row['away_score']: return 0
+    else: return 3
 
 df['winner'] = df.apply(lambda row: winner(row), axis=1)
 
 
 #verifica perdedor
 def loser(row):
-    if row['home_score'] < row['away_score']: return row['home_team']
-    elif row['home_score'] > row['away_score']: return row['away_team']
-    else: return 'DRAW'
+    if row['home_score'] < row['away_score']: return 1
+    elif row['home_score'] > row['away_score']: return 0
+    else: return 3
 
 df['loser'] = df.apply(lambda row: loser(row), axis=1)
+
+
+
+
+dfResultados
+
 
 
 grupo = sys.argv[1]
 print("Grupo")
 print (grupo)
+dict = []
 with open('worldcup2018.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         pais = row[0]
+
         if row[1] != grupo:
             continue
         print(pais)
+
         timeEmcasa = df[df['home_team'] == pais ]
         timeForaCasa = df[df['away_team'] == pais ]
         time = pd.concat([timeEmcasa,timeForaCasa])
-        if time.date.count() == 0:
-            continue;
-        print(estatisticasTime(time,pais))
+
+        vitorias = time[time['winner'] == 1 ]
+        derrotas = time[time['loser']  == 1 ]
+        empates = time[time['winner'] == 3 ]
+
+
+
+        print("vitorias")
+        print(sum(vitorias.winner.values))
+
+        print("derrotas")
+        print(sum(derrotas.loser.values))
+
+        print("empates")
+        print(sum(empates.winner.values))
+
+        if pais == 'Russia':
+            print("Gols em casa")
+            #print(time.home_score.values)
+            score = time[time['home_score'] != 0]
+            print(statistics.median(score.home_score.values))
+            print(statistics.variance(score.home_score.values))
+
+        else:
+            print("Gols fora de casa")
+            #print(time.away_score.values)
+            score = time[time['away_score'] != 0]
+            print(statistics.median(score.away_score.values))
+            print(statistics.variance(score.away_score.values))
+        print("\n")
+        print("--------------------------")
